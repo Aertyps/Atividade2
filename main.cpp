84d6703d0160e7d53d46ff99e4c096a09109e104
AERTY  PINTO DOS SANTOS <>
2021-05-21 00:37:51 +0000

    inicial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+int mudaMsg(int*n1,int*n2){
+  
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+  int num1 = 0,num2 = 0;
+  string msg;
+  char substituir,substituto; 
+  cout << "Digite uma mensagem: " << endl;
+  cin >> msg;
+  cout << "Digite um caractere a ser substituído.: " << endl;
+  cin >> num2;
+  cout << "Digite um caractere a ser colocado no lugar do anterior: " << endl;
+  cin >> num2;
+  
+ 
+
 }
