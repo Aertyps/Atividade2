4c847183196f17bc3aac0e6f2e2c4dfea457ad11
AERTY  PINTO DOS SANTOS <>
2021-05-21 00:43:48 +0000

    ini2

--- main.cpp
+++ main.cpp
@@ -2,20 +2,19 @@
 
 using namespace std;
 
-int mudaMsg(int*n1,int*n2){
+int mudaMsg(string* msg, char s1, char s2, string* msg2){
   
 }
 
 int main() {
-  int num1 = 0,num2 = 0;
   string msg;
   char substituir,substituto; 
   cout << "Digite uma mensagem: " << endl;
   cin >> msg;
-  cout << "Digite um caractere a ser substituído.: " << endl;
-  cin >> num2;
+  cout << "Digite um caractere a ser substituído" << endl;
+  cin >> substituir;
   cout << "Digite um caractere a ser colocado no lugar do anterior: " << endl;
-  cin >> num2;
+  cin >> substituto;
   
  
 
